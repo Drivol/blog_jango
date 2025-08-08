d61efb95e01b259e6147d2023626af9ece1972a3
Kaua <kaua.fmelo@hotmail.com>
2025-08-07 21:20:06 -0300

    criado os primeiros passos do django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    subtitulo = models.TextField()
+    informações = models.TextField()
+
+
+def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+# recipes/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
